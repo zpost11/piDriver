c9173cb456af10f1d18613442a8085329bcd5696
John Doe <johndoe@example.com>
2025-07-04 13:19:57 -0600

    changed to match example better removed gpiod put

--- 03_gpioCtrl/gpioCtrl.c
+++ 03_gpioCtrl/gpioCtrl.c
@@ -5,53 +5,52 @@
 static struct gpio_desc *led;
 static struct gpio_desc *button;
 
-const int PinOffset = 512;
-const int IO_LED = 21 + PinOffset;
-const int IO_BUTTON = 20 + PinOffset;
+const int offset = 512;
+const int IO_LED =  21 + offset;
+const int IO_BUTTON = 20 + offset;
 
 static int __init my_init(void)
 {
-    int status = 0;
+	int status = 0;
 
-    led = gpio_to_desc(IO_LED);
-    if (!led) 
+	led = gpio_to_desc(IO_LED);
+	if (!led) 
     {
-        printk("Failed to get GPIO descriptor for LED\n");
-        return -ENODEV;
-    }
+		printk("gpioctrl - Error getting pin 21\n");
+		return -ENODEV;
+	}
 
-    button = gpio_to_desc(IO_BUTTON);
-    if (!button) 
+	button = gpio_to_desc(IO_BUTTON);
+	if (!button) 
     {
-        printk("Failed to get GPIO descriptor for Button\n");
-        gpiod_put(led);
-        return -ENODEV;
-    }
+		printk("gpioctrl - Error getting pin 20\n");
+		return -ENODEV;
+	}
 
-    status = gpiod_direction_output(led, 0);
-    if(status)
+	status = gpiod_direction_output(led, 0);
+	if (status) 
     {
-        printk("Failed to set LED direction\n");
-        return status;
-    }
+		printk("gpioctrl - Error setting pin 20 to output\n");
+		return status;
+	}
 
-    status = gpiod_direction_input(button);
-    if (status) 
+	status = gpiod_direction_input(button);
+	if (status) 
     {
-        printk("Failed to set Button direction\n");
-        return status;
-    }
+		printk("gpioctrl - Error setting pin 21 to input\n");
+		return status;
+	}
 
-    gpiod_set_value(led, 1);
-    printk("gpioctrl - button is %spressed\n", gpiod_get_value(button) ? "": "not ");
+	gpiod_set_value(led, 1);
 
-    return 0;
+	printk("gpioctrl - Button is %spressed\n", gpiod_get_value(button) ? "" : "not ");
+
+	return 0;
 }
 
 static void __exit my_exit(void)
 {
-    gpio_set_value(led, 0);
-    printk("Goodbye - Goodbye, Kernel!\n");
+	gpiod_set_value(led, 0);
 }
 
 module_init(my_init);
@@ -59,4 +58,4 @@ module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("John");
-MODULE_DESCRIPTION("A simple GPIO control module");
+MODULE_DESCRIPTION("An example of GPIO control");
