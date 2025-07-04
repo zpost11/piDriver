66d10b7d1f17db986fdac031d2bfcb85ccffd8e7
John Doe <johndoe@example.com>
2025-07-04 14:57:25 -0600

    Created

--- 05_hello_cdev/hello_cdev.c
+++ 05_hello_cdev/hello_cdev.c
@@ -2,24 +2,35 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 
-static int major = 0;
-static struct file_operations fops = {};
+static int major;
+
+static ssize_t my_read(struct file *f, char __user *u, size_t l, loff_t *o)
+{
+	printk("hello_cdev - Read is called\n");
+	return 0;
+}
+
+
+static struct file_operations fops = 
+{
+	.read = my_read
+};
 
 static int __init my_init(void)
 {
-    major = register_chrdev(0, "hello_cdev", &fops);
-    if (major < 0) {
-        printk("hello_cdev - error registering\n");
-        return major;
-    }
-    printk("hello_cdev - registered with major number %d\n", major);
-    return 0;
+	major = register_chrdev(0, "hello_cdev", &fops);
+	if (major < 0) 
+    {
+		printk("hello_cdev - Error registering chrdev\n");
+		return major;
+	}
+	printk("hello_cdev - Major Device Number: %d\n", major);
+	return 0;
 }
 
 static void __exit my_exit(void)
 {
-    unregister_chrdev(major, "hello_cdev");
-    printk("Goodbye - Goodbye, Kernel!\n");
+	unregister_chrdev(major, "hello_cdev");
 }
 
 module_init(my_init);
@@ -27,4 +38,4 @@ module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("John");
-MODULE_DESCRIPTION("A simple driver for registering a character device.");
+MODULE_DESCRIPTION("A sample driver for registering a character device");
